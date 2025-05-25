74df24f4824cb5e3b74cb3c12c354949d74c3fde
ogakei4869 <>
2025-05-25 11:32:17 +0900

    build

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4243744: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4243805: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4243869: function() {return Module.webglContextAttributes.powerPreference;},  
- 4243927: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4243982: function($0) {performance.now = function() { return $0; };},  
- 4244030: function($0) {performance.now = function() { return $0; };},  
- 4244078: function() {performance.now = Module['emscripten_get_now_backup'];}
+  4243552: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4243613: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4243677: function() {return Module.webglContextAttributes.powerPreference;},  
+ 4243735: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4243790: function($0) {performance.now = function() { return $0; };},  
+ 4243838: function($0) {performance.now = function() { return $0; };},  
+ 4243886: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
